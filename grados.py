b805ec341b276244f99d5cb8a91fb027898f3512
pollocandente <a.23308051281211@cbtis128.edu.mx>
2024-09-23 20:45:49 -0600

    Add files via upload

---
+++ grados.py
@@ -0,0 +1,10 @@
+print(" ")#define una linea en blanco
+print("Roman De la Cruz Leonardo Antonio, grados")
+
+#solicita valores
+fahrenheit = float(input("Ingrese la temperatura:"))
+
+print(" ")#define una linea en blanco
+celsius = (5/9) * (fahrenheit - 32)#operacion usada
+print(" ")#define una linea en blanco
+print(f"La temperatura es", celsius)#resultado
